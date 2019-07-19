d9971cf560a600dd8c36d07b711a2d8c9bff64b5
soldierlee <bsoldierlee1024@gmail.com>
2019-07-20 02:50:26 +0800

    添加抽象方法模式

---
+++ src/creation_pattern/FactoryPattern.java
@@ -0,0 +1,57 @@
+package creation_pattern;
+
+/**
+ * 
+ * @author soldierlee
+ * @email bsoldier1024@gmail.com
+
+ * 2019年7月20日 上午2:45:46
+ */
+public class FactoryPattern {
+
+	public static void main(String[] args) {
+		DogFactory df = new DogFactory();
+		Dog d = (Dog) df.createAnimal();
+		d.eat();
+	}
+
+	
+}
+
+interface Factory {
+	public Animal createAnimal();
+}
+
+
+class CatFactory implements Factory {
+	public Animal createAnimal() {
+		return new Cat();
+	}
+	
+}
+class DogFactory implements Factory {
+	public Animal createAnimal() {
+		return new Dog();
+	}
+}
+
+abstract class Animal {
+	public abstract void eat();
+}
+
+
+class Cat extends Animal {
+
+	@Override
+	public void eat() {
+		System.out.println("猫吃老鼠...");
+	}
+	
+}
+
+class Dog extends Animal {
+	@Override
+	public void eat() {
+		System.out.println("狗吃肉...");
+	}
+}
